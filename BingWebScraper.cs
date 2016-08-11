06167d8be3f89d66041465328c9a14a85b620873
didmau5 <mow.daniel@gmail.com>
2016-08-11 15:57:52 -0700

    Initial Commit

---
+++ BingWebScraper.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+
+namespace ConsoleWebScraper
+{
+    class BingWebScraper
+    {
+        
+        public static string BING_URL = "http://www.bing.com/news/search?q=brexit";
+        public static string LINK_URL_FILTER = "reuters";
+        public static string LINK_CONTENT_FILTER = "Hayley Platt";
+		
+        //Given a list of strings, prints each
+        static void printStringList(List<string> strList)
+        {
+            Console.WriteLine(String.Join("\n ", strList.ToArray()));
+        }
+
+        //Given a URL, extracts html as string
+        static string getHtml(string url)
+        {
+            string html = "";
+            //Console.WriteLine("Requesting ... " + url);
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream responseStream = response.GetResponseStream();
+                StreamReader responseReader = new StreamReader(responseStream);
+
+                html = responseReader.ReadToEnd();
+            }
+            catch(WebException e)
+            {
+                WebResponse response = e.Response;
+                HttpWebResponse httpResponse = (HttpWebResponse)response;
+                Console.WriteLine("HTTP ERROR CODE:\n {0}\n", httpResponse.StatusCode);
+            }
+            
+            return html;
+
+        }
+
+        //Given html string, returns links as List object
+        static List<string> getLinks(string html)
+        {
+            List<string> links = new List<string>();
+
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(html);
+
+            foreach(HtmlNode link in doc.DocumentNode.SelectNodes("//a[@href]"))
+            {
+                string linkString = link.GetAttributeValue("href", string.Empty);
+                if(linkString.StartsWith("http://www.") || linkString.StartsWith("https://www."))
+                {
+                    links.Add(linkString);
+                }
+            }
+            return links;
+
+        }
+        
+        //Given List of strings and keyword, returns filtered list based on url
+        static List<string> filterLinksByUrl(List<string> sourceLinks, string keyword)
+        {
+            List<string> targetLinks = new List<string>();
+            foreach (var link in sourceLinks)
+            {
+                if (link.Contains(keyword))
+                {
+                    targetLinks.Add(link);
+                }
+            }
+            return targetLinks;
+
+        }
+
+        //Given List strings and keyword, returns filtered list based on html content at links
+        static List<string> filterLinksByContent(List<string> sourceLinks, string keyword)
+        {
+            List<string> targetLinks = new List<string>();
+            foreach (var link in sourceLinks)
+            {
+                string html = getHtml(link);
+                if(html.Contains(keyword))
+                {
+                    targetLinks.Add(link);
+                }
+            }
+            return targetLinks;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Begining Web Scraper...\n");
+         
+            //Get Bing News Search Result Links for "Brexit"
+            string bingHtml = getHtml(BING_URL);
+            List<string> bingLinks  = getLinks(bingHtml);
+
+            //Get Links to Reuters
+            List<string> reutersLinks = filterLinksByUrl(bingLinks, LINK_URL_FILTER);
+
+            //Search Reuters Pages for Hayley Platt
+            List<string> haleyPlattLinks = filterLinksByContent(reutersLinks, LINK_CONTENT_FILTER);
+
+            //Print list of links mentioning Hayley Platt
+            Console.WriteLine("Program Output:\n");
+            printStringList(haleyPlattLinks);
+        }
+    }
+}
---
+++ FourWayIntersectionSimulation.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic; 
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ConsoleApplication
+{
+    //contains road state
+    class Road
+    {
+        public bool vehiclePresent;
+        public bool light;
+        public int priority;
+        public string direction;
+        
+
+        public Road(string dir, Random randomGenerator)
+        {
+            direction = dir;
+            vehiclePresent = (randomGenerator.Next(2) == 0);
+            light = false;
+            priority = 0;
+        }
+
+        public void getNewVehiclePresentState(Random randomGenerator)
+        {
+            vehiclePresent = (randomGenerator.Next(2) == 0);
+        }
+        public void incrementPriority()
+        {
+            priority += 1;
+        }
+    }
+
+    class FourWayIntersectionSimulation
+    {
+        public static Road[] intersection = new Road[4];
+        public static readonly Random rnd = new Random();
+
+        static void initializeIntersection()
+        {
+            string[] directions = new string[] {"North","East","South","West" };
+            for(int i = 0;i<intersection.Length;i++)
+            {
+                Road newRoad = new Road(directions[i],rnd);
+                intersection[i] = newRoad;
+            }
+        }
+
+        static void printIntersectionState()
+        {
+            for(int i = 0; i<intersection.Length; i++)
+            {
+                string vPreseneceString;
+                string lightColourString;
+                if(intersection[i].vehiclePresent)
+                {
+                    vPreseneceString = "Vehicle Present";
+                }
+                else
+                {
+                    vPreseneceString = "Vehicle Not Present";
+                }
+                if (intersection[i].light)
+                {
+                    lightColourString = "Green - GO!";
+                }
+                else
+                {
+                    lightColourString = "Red";
+                }
+                Console.WriteLine(intersection[i].direction + ": \n\t=>" + vPreseneceString + "\n\t=>" + lightColourString + "\n\t=>Priority:\n\t\t" + intersection[i].priority);
+            }
+            Console.WriteLine("============\n\n");
+        }
+
+        //return the highest priority value in the intersection
+        static int getHighestPriority()
+        {
+            int highest = 0;
+            for(int i = 0; i<intersection.Length; i++)
+            {
+                if(intersection[i].priority > highest)
+                {
+                    highest = intersection[i].priority;
+                }
+            }
+            return highest;
+        }
+
+        static void Main()
+        {
+            initializeIntersection();
+
+            Console.WriteLine("Four Way Intersection Simulation.  \'q\' + enter to exit program.\n\n");
+
+            System.Timers.Timer aTimer = new System.Timers.Timer();
+            aTimer.Elapsed += new ElapsedEventHandler(updateState);
+            aTimer.Interval = 10000;
+            aTimer.Enabled = true;
+            while (Console.Read() != 'q') ;
+
+        }
+
+        //set vehicle to moving into intersection
+        static void Go(Road throughVehicle)
+        {
+            throughVehicle.getNewVehiclePresentState(rnd);
+            throughVehicle.light = true;
+            throughVehicle.priority = 0;
+        }
+
+        private static void updateState(object source, ElapsedEventArgs e)
+        {
+            printIntersectionState();
+            int hp = getHighestPriority();
+            bool clear = true;
+            
+            for(int i = 0; i<intersection.Length; i++)
+            {
+                intersection[i].light = false;
+                //is there a vehicle present?
+                if(intersection[i].vehiclePresent)
+                {
+                    //are we highest priority?
+                    if(intersection[i].priority == hp)
+                    {
+                        //intersection clear - GO!
+                        if (clear)
+                        {
+                            clear = false;
+                            Go(intersection[i]);
+                        }
+                        //car in intersection - wait
+                        else
+                        {
+                            intersection[i].incrementPriority();
+                        }
+                    }
+                    //were not highest priority - wait
+                    else
+                    {
+                        intersection[i].incrementPriority();
+                    }
+                }
+                //no vehicle present - get new state
+                else
+                {
+                    intersection[i].getNewVehiclePresentState(rnd);
+                }
+            }
+        }
+    }
+}
